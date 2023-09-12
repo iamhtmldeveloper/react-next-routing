ca8515bba0ef487bd5d14970e3227e752337e2f1
Amol Bhandare <anmolhtmldeveloper@gmail.com>
2023-09-12 13:41:49 +0530

    Next simple routing with multiple pages

---
+++ app/page.js
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+import Home from "@/app/Home/page"
+
+const page = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+export default page
